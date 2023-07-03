18c0d92d01feb886cc8ff0380498eb20e28eaff9
Viktor <gievoi.v@gmail.com>
2023-07-03 15:36:33 +0200

    Fix

--- src/Domain/Factory/UUID/Generator/SimpleGenerator.php
+++ src/Domain/Factory/UUID/Generator/SimpleGenerator.php
@@ -4,20 +4,12 @@ declare(strict_types = 1);
 
 namespace App\Domain\Factory\UUID\Generator;
 
+use Ramsey\Uuid\Uuid;
+
 final class SimpleGenerator implements GeneratorInterface
 {
     public function generate(): string
     {
-        return sprintf(
-            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0x0fff) | 0x4000,
-            mt_rand(0, 0x3fff) | 0x8000,
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff)
-        );
+        return Uuid::uuid4()->toString();
     }
 }
