8da2cc7a6acf105ec03ff2589b8ade560f67f9a6
Viktor <gievoi.v@gmail.com>
2023-07-07 21:19:36 +0200

    Fix

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -20,7 +20,7 @@ fi
 /app/bin/console cache:warmup > /dev/null 2>&1 && stat "Warmup cache" "OK" || stat "Failed to warmup cache" "FAIL"
 chown www-data:www-data -R /app/var > /dev/null 2>&1 && stat "Fix var chown" "OK" || stat "Failed to change var directory owner" "FAIL"
 
-if [ ! -f "resource/production.db" ]; then
+if [ ! -f "/app/resource/production.db" ]; then
     /app/bin/console do:da:cr > /dev/null 2>&1 && stat "Creating empty database" "OK" || stat "Failed to create database" "FAIL"
     /app/bin/console do:sc:up --force --complete > /dev/null 2>&1 && stat "Update database schema" "OK" || stat "Failed to update database schema" "FAIL"
     /app/bin/console do:fi:lo --no-interaction > /dev/null 2>&1 && stat "Load default fixtures" "OK" || stat "Failed to load default fixtures" "FAIL"
@@ -29,7 +29,7 @@ else
     /app/bin/console do:sc:up --force > /dev/null 2>&1 && stat "Update database schema" "OK" || stat "Failed to update database schema" "FAIL"
 fi
 
-chown www-data:www-data /app/resource/*.db && stat "Update database permissions" "OK" || stat "Failed to update database permissions" "FAIL"
+chown www-data:www-data /app/resource/production.db && stat "Update database permissions" "OK" || stat "Failed to update database permissions" "FAIL"
 
 php-fpm -D > /dev/null 2>&1 && stat "Running PHP-FPM" "OK" || stat "Failed to run PHP-FPM" "FAIL"
 
